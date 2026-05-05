3ed4ae0c9f13a58cd3ea5854b5ff7cc1df9350f5
pajetic <mssjimmy@gmail.com>
2026-05-04 23:26:23 -0400

    More collisions + cinemachine

---
+++ Assets/Scripts/Collision.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+    }
+}
