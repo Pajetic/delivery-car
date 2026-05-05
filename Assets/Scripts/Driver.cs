e8d4799da220ba3b767f1963983f2f82266a0bf2
pajetic <mssjimmy@gmail.com>
2026-05-04 23:14:53 -0400

    Basic movement + colliders

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField] float turnRate = 100f;
+    [SerializeField] float moveSpeed = 10f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,7 +14,31 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0, 0, 1), Time.deltaTime * 100f);
-        transform.Translate(new Vector3(0, Time.deltaTime * 10f, 0));
+        float move = 0f;
+        float turn = 0f;
+        
+        if (Keyboard.current.wKey.isPressed)
+        {
+            move = 1f;
+        }
+        else if (Keyboard.current.sKey.isPressed)
+        {
+            move = -1f;
+        }
+        
+        if (Keyboard.current.aKey.isPressed)
+        {
+            turn = 1f;
+        }
+        else if (Keyboard.current.dKey.isPressed)
+        {
+            turn = -1f;
+        }
+        
+        float moveAmount = move * moveSpeed * Time.deltaTime;
+        float turnAmount = turn * turnRate * Time.deltaTime;
+        
+        transform.Translate(new Vector3(0, moveAmount, 0));
+        transform.Rotate(new Vector3(0, 0, turnAmount));
     }
 }
